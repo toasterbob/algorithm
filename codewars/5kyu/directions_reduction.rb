9ca1ad87b1a60deb4436346c26a2b41a7421affe
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-02 15:17:11 -0800

    Directions Reduction

---
+++ codewars/5kyu/directions_reduction.rb
@@ -0,0 +1,18 @@
+def dirReduc(arr)
+  sorted = true
+  (arr.length - 1).times do |i|
+    cond1 = arr[i] == "NORTH" && arr[i+1] == "SOUTH"
+    cond2 = arr[i] == "SOUTH" && arr[i+1] == "NORTH"
+    cond3 = arr[i] == "WEST" && arr[i+1] == "EAST"
+    cond4 = arr[i] == "EAST" && arr[i+1] == "WEST"
+    if cond1 || cond2 || cond3 || cond4
+      arr.slice!(i, 2)
+      sorted = false
+    end
+  end
+  if sorted
+    arr
+  else
+    dirReduc(arr)
+  end
+end
