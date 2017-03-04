00ea479e5e4bd77a22cc94dcebf2152e35320021
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-03 22:40:36 -0800

    bookseller

--- codewars/6kyu/bookseller.rb
+++ codewars/6kyu/bookseller.rb
@@ -1,13 +1,16 @@
 def stockList(listOfArt, listOfCat)
   result = []
+  any_results = false
+  
   listOfArt.map! { |pair| pair.split(" ") }
   listOfArt.map! { |arr| [arr[0], arr[1].to_i] }
 
-  p listOfArt
   listOfCat.each do |cat|
     total = 0
     listOfArt.each {|arr| total += arr[1] if arr[0][0] == cat}
-    result << "(#{cat} : #{total})" if total > 0
+    result << "(#{cat} : #{total})"
+    any_results = true if total > 0
   end
-  result.join(" - ")
+
+  any_results ? result.join(" - ") : ""
 end
