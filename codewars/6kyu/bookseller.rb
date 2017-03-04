d9279be2f5bac3e88f60c3ae41b175b4b8ed9026
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-03 23:02:49 -0800

    bookseller

--- codewars/6kyu/bookseller.rb
+++ codewars/6kyu/bookseller.rb
@@ -1,42 +1,22 @@
-# def stockList(listOfArt, listOfCat)
-#   result = []
-#   any_results = false
-#
-#   listOfArt.map! { |pair| pair.split(" ") }
-#   listOfArt.map! { |arr| [arr[0], arr[1].to_i] }
-#
-#   listOfCat.each do |cat|
-#     total = 0
-#     listOfArt.each {|arr| total += arr[1] if arr[0][0] == cat}
-#     result << "(#{cat} : #{total})"
-#     any_results = true if total > 0
-#   end
-#
-#   any_results ? result.join(" - ") : ""
-# end
 
 def stockList(listOfArt, listOfCat)
+  return "" if listOfArt.empty? || listOfCat.empty?
+
   result = []
-  any_results = false
-
-  listOfArt.map! { |pair| pair.split(" ") }
-  listOfArt.map! { |arr| [arr[0], arr[1].to_i] }
-
   hash = {}
 
   listOfArt.each do |arr|
-    code, amount = arr
+    code, amount = arr.split(" ")
     if hash[code[0]]
-      hash[code[0]] += amount
+      hash[code[0]] += amount.to_i
     else
-      hash[code[0]] = amount
+      hash[code[0]] = amount.to_i
     end
   end
 
   listOfCat.each do |cat|
-    any_results = true if hash[cat]
     hash[cat] = 0 if hash[cat].nil?
     result << "(#{cat} : #{hash[cat]})"
   end
-  any_results ? result.join(" - ") : ""
+  result.join(" - ")
 end
