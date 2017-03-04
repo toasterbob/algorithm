6e01bb699bd71c952ccdcfa830e5fc7c1b904f4d
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-03 19:12:15 -0800

    Caesar Cipher2

---
+++ codewars/5kyu/caesar_cipher2.rb
@@ -0,0 +1,24 @@
+def movingShift(s, shift)
+    coded = ""
+    s.chars do |ch|
+      if ch.downcase >= "a" && ch.downcase <= "z"
+        shift = shift % 26
+        shift += 26 if shift < 0 #for negative shifts
+        shift.times {ch.next!}
+      end
+      coded << ch[-1]
+      shift += 1
+    end
+    arr = []
+    divide = s.length/5
+    divide += 1 if divide * 5 < s.length
+    5.times do |i|
+      arr << coded.slice!(0, divide)
+    end
+    arr
+end
+
+def demovingShift(arr, shift)
+    coded = ""
+
+end
