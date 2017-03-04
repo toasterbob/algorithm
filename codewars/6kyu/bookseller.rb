4dac0d030d7e513b8c89dd6e2b3ab1a6ebe2800a
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-03 23:07:08 -0800

    bookseller

--- codewars/6kyu/bookseller.rb
+++ codewars/6kyu/bookseller.rb
@@ -2,21 +2,13 @@
 def stockList(listOfArt, listOfCat)
   return "" if listOfArt.empty? || listOfCat.empty?
 
-  result = []
-  hash = {}
+  hash = Hash.new(0)
 
   listOfArt.each do |arr|
     code, amount = arr.split(" ")
-    if hash[code[0]]
-      hash[code[0]] += amount.to_i
-    else
-      hash[code[0]] = amount.to_i
-    end
+    hash[code[0]] += amount.to_i
   end
 
-  listOfCat.each do |cat|
-    hash[cat] = 0 if hash[cat].nil?
-    result << "(#{cat} : #{hash[cat]})"
-  end
-  result.join(" - ")
+  listOfCat.map! { |cat| "(#{cat} : #{hash[cat]})" }.join(" - ")
+
 end
