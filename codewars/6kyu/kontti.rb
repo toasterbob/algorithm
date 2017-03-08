6eb705ed1858bcccd0527af1336e839bca0330f4
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-07 16:37:02 -0800

    kontti

---
+++ codewars/6kyu/kontti.rb
@@ -0,0 +1,14 @@
+class String
+  def kontti
+    result = []
+    self.split(" ").each do |word|
+
+      if /(?=.*[aeiouyAEIOUY])/.match(word)
+
+      else
+        result << word
+      end
+    end
+    result.join(" ")
+  end
+end
