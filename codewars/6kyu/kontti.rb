4a0aeb6c52c3343e808dc4dc9fb9034094c011f5
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-07 16:43:09 -0800

    kontti

--- codewars/6kyu/kontti.rb
+++ codewars/6kyu/kontti.rb
@@ -4,12 +4,16 @@ class String
 
     self.split(" ").each do |word|
       if /(?=.*[aeiouyAEIOUY])/.match(word)
-
+        trade = "kontii"
+        word.length.times do |i|
+          break if i > 5 #bigger than kontti length
+          
+        end
       else
         result << word
       end
     end
-    
+
     result.join(" ")
   end
 end
