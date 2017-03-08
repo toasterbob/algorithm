f6c91f631f3cfbea8ce79ef0a064f6559de99089
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-07 17:26:29 -0800

    kontti

--- codewars/6kyu/kontti.rb
+++ codewars/6kyu/kontti.rb
@@ -8,7 +8,7 @@ class String
         breakpoint = nil
         word.length.times do |i|
           breakpoint = i
-          break if %w(a e i o u y).include?(word[i])
+          break if %w(a e i o u y).include?(word[i].downcase)
         end
         result << "#{trade[0, 2]}#{word[breakpoint + 1, word.length]}-#{word[0, breakpoint+1]}#{trade[2, trade.length]}"
       else
