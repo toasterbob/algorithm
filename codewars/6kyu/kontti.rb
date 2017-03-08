1b47bed4cf070930b7c2f9961bd32e0f630cf542
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-07 17:19:32 -0800

    kontti

--- codewars/6kyu/kontti.rb
+++ codewars/6kyu/kontti.rb
@@ -10,7 +10,7 @@ class String
           breakpoint = i
           break if %w(a e i o u y).include?(word[i])
         end
-        result << "#{trade[0, 2]}#{word[2, word.length]}-#{word[0, breakpoint+1]}#{trade[2, trade.length]}"
+        result << "#{trade[0, 2]}#{word[breakpoint + 1, word.length]}-#{word[0, breakpoint+1]}#{trade[2, trade.length]}"
       else
         result << word
       end
