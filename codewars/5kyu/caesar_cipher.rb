0a217ae70e4882cd6690fdb57d09c20f5431d662
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-08 16:50:57 -0800

    caesar cipher

--- codewars/5kyu/caesar_cipher.rb
+++ codewars/5kyu/caesar_cipher.rb
@@ -2,14 +2,11 @@ def encode_str(string, shift)
    result = []
    shift = shift % 26
    string.length.times do |i|
-     if (i >= "a" && i <= "z") || (i >= "A" && i <= "Z")
+
+     if (string[i] >= "a" && string[i] <= "z") || (string[i] >= "A" && string[i] <= "Z")
        shift.times { string[i] = (string[i].next)[0] }
      end
    end
 
    string = "#{string[0].downcase}#{string}"
-end
-
-def decode(arr)
-
 end
