d359c5bce077e1e796cb4f67969fdcb17d4bd9ea
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-08 17:27:58 -0800

    caesar cipher

--- codewars/5kyu/caesar_cipher.rb
+++ codewars/5kyu/caesar_cipher.rb
@@ -20,7 +20,7 @@ def decode(arr)
   first, second = arr[0].slice!(0,1), arr[0].slice!(0,1)
   n = (second.ord - first.ord) % 26
   arr.join("")
-  encode(arr.join, n)
+  encode_str(arr.join, n).join[2, -1]
 end
 
 # def decode(arr)
